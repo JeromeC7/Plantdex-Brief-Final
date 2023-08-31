c1afb732ed326ac7e5a69a1f1373ca8f8e875583
JeromeC7 <jerome.cornet@sncf.fr>
2023-08-31 13:54:26 +0200

    7.4 Token local storage

--- src/app/pages/page-admin/page-admin.component.ts
+++ src/app/pages/page-admin/page-admin.component.ts
@@ -9,29 +9,36 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
   styleUrls: ['./page-admin.component.css']
 })
 export class PageAdminComponent {
- loginForm: FormGroup;
-
-constructor(private formBuilder: FormBuilder,private instanceUserService: UserService, private activatedRoute:ActivatedRoute, private router:Router) {
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder,private instanceUserService: UserService, private activatedRoute:ActivatedRoute, private router:Router) {
     this.loginForm = new FormGroup({
-        email: new FormControl(''),
-        password: new FormControl(''),
-      });
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
 
   ngOnInit() {
-  this.loginForm = this.formBuilder.group({
+    if (localStorage.getItem('token')) {
+      console.log("TOKEN présent dans localStorage");
+    }else{
+      console.log("TOKEN absent de localStorage");
+    }
+
+    this.loginForm = this.formBuilder.group({
       email: ['admin@gmail.com', Validators.required],
       password: ['', Validators.required], // Initialize with an empty string and add any validators you need
       });
-   }
+  }
 
-   login(){
-      const loginForm = this.loginForm;
-      const data = {"email": loginForm.get('email')?.value,"password": loginForm.get('password')?.value};
-      this.instanceUserService.login(data).subscribe((response: any) => {
-        console.log('User connecté!'+response);
-        console.log("token = "+response.data);
-      });
+  login(){
+    const loginForm = this.loginForm;
+    const data = {"email": loginForm.get('email')?.value,"password": loginForm.get('password')?.value};
+    this.instanceUserService.login(data).subscribe((response: any) => {
+      console.log('User connecté!'+response);
+      console.log("token = "+response.data);
+      // Store the token in local storage
+      localStorage.setItem('token', response.data);        
+    });
   }
 }
 
